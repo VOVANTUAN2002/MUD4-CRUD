33b4080cd9ef759c5841012099cfc68545f59f96
vovantuan2002 <votuant2@gmail.com>
2022-05-25 11:44:28 +0700

    crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { BookDeleteComponent } from './book-delete/book-delete.component';
+import { BookShowComponent } from './book-show/book-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookAddComponent,
+    BookEditComponent,
+    BookDeleteComponent,
+    BookShowComponent
   ],
   imports: [
     BrowserModule
